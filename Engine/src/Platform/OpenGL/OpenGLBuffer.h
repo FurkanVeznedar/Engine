6cebd57cfe9a95669931bc4e11b161d4ebc0a361
FurkanVeznedar <furkanveznedar@gmail.com>
2024-04-29 01:08:13 +0300

    VertexBufferLayout abstraction is completed

--- Engine/src/Platform/OpenGL/OpenGLBuffer.h
+++ Engine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -11,10 +11,14 @@ namespace Engine {
         OpenGLVertexBuffer(float* vertices, uint32_t size);
         virtual ~OpenGLVertexBuffer();
 
-        virtual void Bind() const;
-        virtual void UnBind() const;
+        virtual void Bind() const override;
+        virtual void UnBind() const override;
+
+        virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+        virtual void SetLayout(const BufferLayout layout) override { m_Layout = layout; }
     private:
         uint32_t m_RendererID;
+        BufferLayout m_Layout;
     };
 
     class OpenGLIndexBuffer : public IndexBuffer
@@ -23,10 +27,10 @@ namespace Engine {
         OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
         virtual ~OpenGLIndexBuffer();
 
-        virtual void Bind() const;
-        virtual void UnBind() const;
+        virtual void Bind() const override;
+        virtual void UnBind() const override;
 
-        virtual uint32_t GetCount() const;
+        virtual uint32_t GetCount() const override;
     private:
         uint32_t m_RendererID;
         uint32_t m_Count;
